c0d2bf0caadf107d63f37a48d2c485f4669c36a8
L00MYY <160758553+L00MYY@users.noreply.github.com>
2026-05-28 21:55:18 -0600

    Se hicieron los llamados a las funciones del ejercicio 1

--- Ejercicio 1 - Historial de navegacion/Program.cs
+++ Ejercicio 1 - Historial de navegacion/Program.cs
@@ -7,3 +7,20 @@ history.VisitPage("google.com");
 history.VisitPage("github.com");
 history.VisitPage("docs.microsoft.com");
 history.PrintHistory();
+// >>> docs.microsoft.com (pagina actual)
+
+// Se navega hacia atras dos veces
+Console.WriteLine(history.GoBack()); // >>> github.com
+Console.WriteLine(history.GoBack()); // >>> google.com
+history.PrintHistory();
+// >>> google.com (pagina actual)
+
+//Se intenta ir mas atras (borde: primera pagina)
+Console.WriteLine(history.GoBack());
+
+//Se navega hacia adelante una vez
+Console.WriteLine(history.GoForward()); // >>> github.com
+history.PrintHistory();
+// >>> github.com (pagina actual)
+
+Console.ReadKey();
